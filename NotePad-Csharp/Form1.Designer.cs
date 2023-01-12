d33ae99e110c4d2f6beef087f5b9c2c07b5d3a44
Sirajum Munir Tamim <asmtamim19@gmail.com>
2023-01-13 01:18:58 +0600

    Add files via upload

---
+++ NotePad-Csharp/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace NotePad_Csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.noteBox = new System.Windows.Forms.TextBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.dgvNote = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNote)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Note";
+            // 
+            // titleBox
+            // 
+            this.titleBox.Location = new System.Drawing.Point(74, 19);
+            this.titleBox.Multiline = true;
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(600, 30);
+            this.titleBox.TabIndex = 0;
+            // 
+            // noteBox
+            // 
+            this.noteBox.Location = new System.Drawing.Point(74, 70);
+            this.noteBox.Multiline = true;
+            this.noteBox.Name = "noteBox";
+            this.noteBox.Size = new System.Drawing.Size(600, 450);
+            this.noteBox.TabIndex = 1;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btnNew.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnNew.Location = new System.Drawing.Point(232, 550);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(130, 40);
+            this.btnNew.TabIndex = 2;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.LightGreen;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btnSave.Location = new System.Drawing.Point(378, 550);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(130, 40);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Tomato;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btnDelete.Location = new System.Drawing.Point(885, 550);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(130, 40);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btnRead.Location = new System.Drawing.Point(741, 550);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(130, 40);
+            this.btnRead.TabIndex = 5;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // dgvNote
+            // 
+            this.dgvNote.AllowUserToAddRows = false;
+            this.dgvNote.AllowUserToDeleteRows = false;
+            this.dgvNote.AllowUserToResizeColumns = false;
+            this.dgvNote.AllowUserToResizeRows = false;
+            this.dgvNote.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNote.Location = new System.Drawing.Point(709, 19);
+            this.dgvNote.Name = "dgvNote";
+            this.dgvNote.ReadOnly = true;
+            this.dgvNote.RowHeadersVisible = false;
+            this.dgvNote.RowHeadersWidth = 51;
+            this.dgvNote.RowTemplate.Height = 24;
+            this.dgvNote.Size = new System.Drawing.Size(333, 500);
+            this.dgvNote.TabIndex = 4;
+            this.dgvNote.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvNote_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 653);
+            this.Controls.Add(this.dgvNote);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.noteBox);
+            this.Controls.Add(this.titleBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "NotePad-CS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNote)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.TextBox noteBox;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.DataGridView dgvNote;
+    }
+}
+
---
+++ NotePad-Csharp/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotePad_Csharp
+{
+    public partial class Form1 : Form
+    {
+        DataTable tableList;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tableList = new DataTable();
+
+            tableList.Columns.Add("Title", typeof(string));
+            tableList.Columns.Add("Note", typeof(string));
+
+            dgvNote.DataSource = tableList;
+
+            dgvNote.Columns["Note"].Visible = false;
+            dgvNote.Columns["Title"].Width = 245;
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            titleBox.Clear();
+            noteBox.Clear();
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            tableList.Rows.Add(titleBox.Text, noteBox.Text);
+            btnNew.PerformClick();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            int index = dgvNote.CurrentCell.RowIndex;
+            if (index > -1)
+            {
+                titleBox.Text = tableList.Rows[index].ItemArray[0].ToString();
+                noteBox.Text = tableList.Rows[index].ItemArray[1].ToString();
+
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int index = dgvNote.CurrentCell.RowIndex;
+            tableList.Rows[index].Delete();
+
+        }
+
+        private void dgvNote_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
